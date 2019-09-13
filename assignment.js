8d39ab6beac6caa0f2b43f08edfd1b5840aa7bde
roopamdhoot <49853180+roopamdhoot@users.noreply.github.com>
2019-09-13 17:38:43 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,6 +1,15 @@
-function include(){
-    var arr=['1','2','3']
-var a= arr.includes('1');
-console.log(a);
-}  
-include()
+ //assignment9
+ function remove(array){
+    var arr=[];
+    for(i=0;i< array.length;i++){
+        if(arr.indexOf(array[i]) === -1){
+            arr.push(array[i]);
+        }
+    }
+
+return arr;
+}
+
+var names= ['1','2','3','1','1'];
+var g=remove(names);
+console.log(g);

4146b0dd48796482390d4b59e0ba9214e6022a0c
roopamdhoot <49853180+roopamdhoot@users.noreply.github.com>
2019-09-13 17:36:54 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,8 +1,6 @@
-//assignment7
-    let s=[1,2,3]
-    let y=[4,5,6]
-    function concat(s,y){
-        let z=s.concat(y);
-        console.log(z);
-    }
-        concat(s,y);
+function include(){
+    var arr=['1','2','3']
+var a= arr.includes('1');
+console.log(a);
+}  
+include()
